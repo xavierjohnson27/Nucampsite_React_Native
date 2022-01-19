7117928fc945b78d80980903fdbd5cf7b187eb58
Xavier J <xmj359@gmail.com>
2022-01-19 11:11:24 -0600

    commit

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import Main from './components/MainComponent';
 
 export default function App() {
-  return (
-   <Main/>
-  );
+    return (
+        <Main />
+    );
 }
 
